a586b2f14d648a4d58fed90789d0c1aee43d01ed
Lucie Korená <53037749+korenal@users.noreply.github.com>
2022-07-26 21:43:00 +0200

    Update filtering_ncbi.py

--- filtering_ncbi.py
+++ filtering_ncbi.py
@@ -1,6 +1,14 @@
+"""
+   This script is used to select one specific annotation from 5 different NCBI annotations for each transcript primarily based
+   on the gene's functional annotation and secondary based on the highest bit_score value.
+"""
+
+
 import re
 input_file = open("BLASTP_M_corti.PRJEB510.WBPS15.protein_NCBI.txt", 'r')
 output_file = open ("BLASTP_M_cort.PRJEB510.WBPS15.protein_NCBI_final_lucka.txt", 'a+')
+
+
 
 gene = ""
 concrete_gene = 0
@@ -10,9 +18,9 @@ unknown = ["unknown", "hypothetical", "unnamed"]
 for line in input_file:
     splitter = re.split('\t', line)
     transcript = splitter[0]
-    if last_transcript == transcript and concrete_gene == 1: # transkript s jiz zapsanym genem - ignoruju
+    if last_transcript == transcript and concrete_gene == 1: # transcript with concrete annotation
         continue
-    elif last_transcript == transcript and concrete_gene == 0: # dalsi z nabizenych genu pro dany transkript
+    elif last_transcript == transcript and concrete_gene == 0: # transcript with no concrete annotation
         gene = splitter[2]
         if any(un in gene for un in unknown):
             concrete_gene = 0
@@ -20,10 +28,10 @@ for line in input_file:
             concrete_gene = 1
             output_file.write(line)
     else: # muzu zacit prozkoumavat novy transkript
-        if last_transcript != transcript and concrete_gene == 0 and last_transcript != "": # transcript, ktery ma vsechny geny unknown, tak je treba zapsat ten prvni ulozeny
+        if last_transcript != transcript and concrete_gene == 0 and last_transcript != "": # transcript with all unknown annotations, write the first annotation
             output_file.write(first_line)
         last_transcript = transcript
-        first_line = line # ulozeno pro pripad, ze by vsechny vyskyty obsahovaly unknown
+        first_line = line # the first annotation
         gene = splitter[2]
         if any(un in gene for un in unknown):
             concrete_gene = 0
