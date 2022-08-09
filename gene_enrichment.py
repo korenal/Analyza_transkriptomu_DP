229a02262a039064126b75611effb6b177e2f992
Lucie Korená <53037749+korenal@users.noreply.github.com>
2022-08-09 22:16:34 +0200

    Update gene_enrichment.py

--- gene_enrichment.py
+++ gene_enrichment.py
@@ -1,40 +1,15 @@
-# Loading files
+"""
+    This script makes a gene enrichment analysis using the Fisher´s exact test.
+"""
+
 import pandas as pd
 import xlsxwriter
 from scipy.stats import fisher_exact
 import re
 
-all_diff_expr_genes = open("IV_B_diff_exp_sign.txt", 'r')
-upregulated_genes = open("IV_B_upregulated.txt", 'r')
-downregulated_genes = open("IV_B_downregulated.txt", 'r')
-
-all_mapped = open("IV_B_diff_exp_sign_mapped.txt", 'r')
-up_mapped = open("IV_B_upregulated_mapped.txt", 'r')
-down_mapped = open("IV_B_downregulated_mapped.txt", 'r')
-
-# Preparing an output file
-final_file = xlsxwriter.Workbook("IV_B_KEGG_gene_enrichment_filtered.xlsx")
-worksheet = final_file.add_worksheet()
-
-header_line = ["B_down", "B_down", "B_up", "B_up"]
-first_line = ["num", "pathway", "pathway", "pathway", "pathway", "ALL_(IV_B)", "log2F>2", "fisher_p-value_adj.", "log2F<-2", "fisher_p-value_adj."]
-signification = 0.05
-
-header_format = final_file.add_format({'bold': True, 'text_wrap': True, 'valign':'top', 'border':1})
-highlight_format = final_file.add_format({'bold': True, 'bg_color': 'yellow'})
-
-column = 6
-for word in header_line:
-    worksheet.write(0, column, word, header_format)
-    column += 1
-
-column = 0    
-for word in first_line:
-    worksheet.write(1, column, word, header_format)
-    column += 1
     
-    # Line counter from input files
 def counter (input_file):
+    """Counts the lines which has a KEGG annotation."""
     count = 0
     for line in input_file:
         if line.startswith("M") and len(line.split()) == 2:
@@ -42,15 +17,18 @@ def counter (input_file):
         else:
             continue
     return count
-  
-  def find_num(text):
+
+
+def find_num(text):
+    """Searching for a number in lines in mapped file."""
     point = 0
     while text[text.find("(", point)+1:text.find(")", point)].isdigit() != True:
         point += 1
     return (str(text[text.find("(", point)+1:text.find(")", point)]))
-  
-  # Preprocessing up/down regulated gene files
+
+
 def preprocessing_up_down_mapped_file(input_file):
+    """Preprocessing the file with up/downregulated genes."""
     codes = []
     counts = []
     for line in input_file:        
@@ -59,10 +37,10 @@ def preprocessing_up_down_mapped_file(input_file):
             codes.append(line[:5])
             counts.append(num)
     return(codes, counts)
-  
-  # Preprocessig file with all different expression genes
-#input_file = open("IV_ICR_diff_exp_sign_mapped.txt", 'r')
+
+
 def preprocessing_all_diff_mapped_file(input_file):
+    """Preprocessing the file with mapped all differential expressed genes."""
     line_before = ""
     categories = {}
     subcategories = {}
@@ -103,24 +81,19 @@ def preprocessing_all_diff_mapped_file(input_file):
     del categories['']
     del subcategories['']
     return (categories, subcategories, counts, codes, names)
-  
-  num_all = counter(all_diff_expr_genes)
-num_up = counter(upregulated_genes)
-num_down = counter(downregulated_genes)
 
-
-all_categories, all_subcategories, all_counts, all_codes, all_names = preprocessing_all_diff_mapped_file(all_mapped)
-up_codes, up_counts = preprocessing_up_down_mapped_file(up_mapped)
-down_codes, down_counts = preprocessing_up_down_mapped_file(down_mapped)
 
 def fisher_test(num_all, num_up_or_down, value_in_all, value_in_up_or_down):
+    """Compute the fisher exact test."""
     not_in_up_or_down = value_in_all - value_in_up_or_down
     others_up_or_down = num_up_or_down - value_in_up_or_down
     others_not_in_up_or_down = num_all - others_up_or_down - not_in_up_or_down
     oddsratio, pvalue = fisher_exact([[value_in_up_or_down, not_in_up_or_down], [others_up_or_down, others_not_in_up_or_down]], alternative='greater')
-    return(pvalue)    
-  
-  def write_line(line, row, header, worksheet):
+    return(pvalue)
+
+
+def write_line(line, row, header, worksheet):
+    """Write the concerete line to the output file."""
     column = 0
     for word in line: 
         if header == 1:            
@@ -136,7 +109,9 @@ def fisher_test(num_all, num_up_or_down, value_in_all, value_in_up_or_down):
                 worksheet.write(row, column, word)
             column += 1
             
+
 def writer (worksheet, num, row, category, subcategory, num_subcategory, values, down_values, up_values, names, codes, num_all, num_up, num_down):
+    """Write the final excel file."""
     first_line = [num, category, subcategory, "{:.1f}".format(num_subcategory), subcategory, sum(values), sum(down_values), fisher_test(num_all, num_down, sum(values), sum(down_values)),\
             sum(up_values), fisher_test(num_all, num_up, sum(values), sum(up_values))]
     write_line(first_line, row, 1, worksheet)
@@ -149,17 +124,62 @@ def writer (worksheet, num, row, category, subcategory, num_subcategory, values,
         num += 1
         row += 1
     return (num, row)
-  
-  row = 2
+
+
+# Loading files
+all_diff_expr_genes = open("IV_B_diff_exp_sign.txt", 'r')  # CHANGE THIS
+upregulated_genes = open("IV_B_upregulated.txt", 'r')  # CHANGE THIS
+downregulated_genes = open("IV_B_downregulated.txt", 'r')  # CHANGE THIS
+
+all_mapped = open("IV_B_diff_exp_sign_mapped.txt", 'r')  # CHANGE THIS
+up_mapped = open("IV_B_upregulated_mapped.txt", 'r')  # CHANGE THIS
+down_mapped = open("IV_B_downregulated_mapped.txt", 'r')  # CHANGE THIS
+
+# Preparing an output file
+final_file = xlsxwriter.Workbook("B&IV_gene_enrichment.xlsx")  # CHANGE THIS
+worksheet = final_file.add_worksheet()  # CHANGE THIS
+
+header_line = ["B_down", "B_down", "B_up", "B_up"]  # CHANGE THIS
+first_line = ["num", "pathway", "pathway", "pathway", "pathway", "ALL_(IV_B)", "log2F>2", "fisher_p-value_adj.",
+              "log2F<-2", "fisher_p-value_adj."]
+signification = 0.05
+
+header_format = final_file.add_format({'bold': True, 'text_wrap': True, 'valign': 'top', 'border': 1})
+highlight_format = final_file.add_format({'bold': True, 'bg_color': 'yellow'})
+
+column = 6
+for word in header_line:
+    worksheet.write(0, column, word, header_format)
+    column += 1
+
+column = 0
+for word in first_line:
+    worksheet.write(1, column, word, header_format)
+    column += 1
+
+# Processing the input mapped files
+all_categories, all_subcategories, all_counts, all_codes, all_names = preprocessing_all_diff_mapped_file(
+    all_mapped)
+up_codes, up_counts = preprocessing_up_down_mapped_file(up_mapped)
+down_codes, down_counts = preprocessing_up_down_mapped_file(down_mapped)
+
+# Counts DE genes with KEGG annotation in input files
+num_all = counter(all_diff_expr_genes)
+num_up = counter(upregulated_genes)
+num_down = counter(downregulated_genes)
+
+# Setting the global variables
+row = 2
 num = 1
 pointer = 0
 point = 0
 num_subcategory = 0.0
 
+# Computing part of script with Fisher exact test
 for category in all_categories.keys():
     num_subcategories = all_categories[category]
     num_subcategory += 0.9
-    for i in range(point, point+num_subcategories):
+    for i in range(point, point + num_subcategories):
         num_subcategory += 0.1
         up_values = []
         down_values = []
@@ -168,7 +188,7 @@ for category in all_categories.keys():
         values = []
         subcategory = list(all_subcategories.keys())[i]
         num_types = list(all_subcategories.values())[i]
-        for j in range (pointer, pointer+num_types):
+        for j in range(pointer, pointer + num_types):
             names.append(all_names[j])
             codes.append(int(all_codes[j]))
             values.append(int(all_counts[j]))
@@ -180,14 +200,13 @@ for category in all_categories.keys():
                 down_values.append(int(down_counts[down_codes.index(all_codes[j])]))
             else:
                 down_values.append(0)
-        pointer = pointer+num_types    
-        num, row = writer(worksheet,num, row, category, subcategory, num_subcategory, values, down_values, up_values, names, codes, num_all, num_up, num_down)
+        pointer = pointer + num_types
+        num, row = writer(worksheet, num, row, category, subcategory, num_subcategory, values,
+                                                  down_values, up_values, names, codes, num_all, num_up, num_down)
     point += num_subcategories
-#final_file.close()
-
 
 worksheet.set_column(
-1,
-4,
+    1,
+    4,
     30)
 final_file.close()
